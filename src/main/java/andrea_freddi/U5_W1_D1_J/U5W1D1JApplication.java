b0eeb448c865701272be73df8ccd2c3571f51c15
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-03-17 17:59:39 +0100

    Initial commit

---
+++ src/main/java/andrea_freddi/U5_W1_D1_J/U5W1D1JApplication.java
@@ -0,0 +1,13 @@
+package andrea_freddi.U5_W1_D1_J;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W1D1JApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W1D1JApplication.class, args);
+	}
+
+}
